63721aadd458fad8593eb5fcb083a25737d35233
tomo-makes <tmsojiner+git@gmail.com>
2017-12-16 21:25:46 +0900

    v0.2.0: tested with Polar H7, added peripheral device filtering config

--- README.md
+++ README.md
@@ -1,30 +1,84 @@
 # node-red-contrib-ble-heart-rate
 
-## Retrieve data from BLE heartrate monitors
+A contributed Node-RED node, that retrieve data from BLE heartrate monitors.
+
+
+## Overview
 
 This node connects to a BLE heartrate monitor, retrieve metrics e.g. heartrate, rri etc, and pass them to the next node.
 
 This node supports the standard BLE GATT - which is the heartrate monitor service and characteristic as "180d" and "2a37" accordingly.
 
+## Installation
 
-## tips
+Mostly you can move to `/.node-red` and install this package using `npm` as below.
 
-- 各フェーズのステータスを定義する: [Node-RED日本ユーザ会 : ステータス](https://nodered.jp/docs/creating-nodes/status)
-- node登録用の名称が"-"区切りの必要があった
+```
+$ cd $HOME/.node-red
+$ npm install node-red-contrib-ble-heart-rate
+```
+
+## Usage
+
+- turn on your BLE heart rate moitor device
+- place the `ble heartrate` node on the flow
+- deploy the flow
+- the node status changes to `connected`
+- the node start to transmit `time`, `flag`, `hbr`, `rri1`, `rri2` through `msg.payload` to the next node
+
+example:
+
+```
+{
+	"time": 2017-12-16T13:15:20Z,
+	"flag": 22,
+	"hbr": 73,
+	"rri1": 852,
+	"rri2": null
+};
+```
 
 
-## todo
+### Node status
 
-for the qiita node-red advent calendar on 12/16
+This `ble heartrate` node has below satus which represents BLE connection status with target devices.
 
-- [x] test to deploy on live node-red: 11/25
-- [x] git push (my repo): 11/26
-- [ ] test to connect to a ble monitor
-- [ ] test the debug msg.payloads
-- [ ] test the node indicators
-- [ ] git push (node)
-- [ ] build a end-to-end demo including dashboards
-- [ ] git push (demo)
-- [ ] write them up
+|status|description|
+|---|---|
+|scanning|Scanning BLE devices nearby|
+|discovered|A BLE device is discovered|
+|connected|Connected to a BLE device|
+|disconnected|Disconnected a BLE device|
+|waiting|Waitint for 10 seconds till the next scan|
 
+## Configuration
 
+- Name
+	- Change the name of the node from the default `ble heartrate` to whatever you want.
+- Authorized peripheral address
+	- e.g. xx:xx:xx:xx:xx:xx
+	- default: `null` (Any BLE devices nearby could be connected.)
+
+## License
+
+MIT License
+
+Copyright (c) 2017 tomo-makes (Tomo Masuda)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- contrib-ble-heart-rate.js
+++ contrib-ble-heart-rate.js
@@ -1,19 +1,21 @@
-module.exports = function(RED) {
-  // Init
-  var noble = require('noble');
-  var authorized_ble_peripheral_address = config.authorized_ble_peripheral_address;
-  var serviceUUID = ["180d"];
-  var characteristicUUID = ["2a37"];
+ module.exports = function(RED) {
+  "use strict";
 
-  function bleHeartRate(config) {
+  function bleHeartrate(config) {
     RED.nodes.createNode(this,config);
     var node = this;
+
+    // Init
+    var noble = require('noble');
+    var authorized_ble_peripheral_address = config.authorized_peripheral_address;
+    var serviceUUID = ["180d"];
+    var characteristicUUID = ["2a37"];
 
     // --- BLE handling using noble --- //
     noble
       .on('stateChange', function(state) {
         if (state === 'poweredOn') { 
-          // scan for peripherals broadcasting the heart rate service and use the first one discovered
+          // Scan for peripherals broadcasting the heart rate service and use the first one discovered
           noble.startScanning(serviceUUID);
           console.log("Scanning BLE devices...");
           node.status({fill:"yellow",shape:"dot",text:"scanning"});
@@ -22,14 +24,14 @@ module.exports = function(RED) {
         }
       })
       .on('discover', function(peripheral) {
-        // stop scanning once a peripheral is discovered
+        // Stop scanning once a peripheral is discovered
         noble.stopScanning();
-        console.log("DISCOVERED: " + peripheral);
+        console.log("DISCOVERED: " + peripheral);        
         node.status({fill:"blue",shape:"dot",text:"discovered"});
 
 
-        if (authorized_ble_peripheral_address == null || peripheral.address === authorized_ble_peripheral_address) {
-          // connect to the peripheral
+        if (peripheral.address === authorized_ble_peripheral_address || authorized_ble_peripheral_address == "") {
+          // connect to the peripherals
           peripheral.connect(function(error) {
             console.log("CONNECTED: " + peripheral);
             node.status({fill:"green",shape:"dot",text:"connected"});
@@ -37,9 +39,9 @@ module.exports = function(RED) {
             peripheral.discoverSomeServicesAndCharacteristics(serviceUUID, characteristicUUID, function(error, services, characteristics){
               characteristics[0].notify(true, function(error){
                 characteristics[0].on('data', function(data, isNotification){
-                  // parsing the data
-                  rri1 = (data[3] << 8) + data[2];
-                  rri2 = (data[5] << 8) + data[4];
+                  // parsing RRIs
+                  var rri1 = (data[3] << 8) + data[2];
+                  var rri2 = (data[5] << 8) + data[4];
                   console.log('Flag: ' + data[0] + '  HBR:' + data[1] + '  RRI1:' + rri1 + '  RRI2:' + rri2);
                   var formatted_time = new Date().toISOString();
 
@@ -50,9 +52,7 @@ module.exports = function(RED) {
                     "rri1": rri1,
                     "rri2": rri2
                   };
-
-                  msg.payload = args;
-                  node.send(msg);
+                  node.send({payload: args});
 
                 });
               });
@@ -76,12 +76,12 @@ module.exports = function(RED) {
                 node.status({fill:"yellow",shape:"dot",text:"scanning"});
           }, 10000);
         }
+
       });
   }
 
-  RED.nodes.registerType("ble-heart-rate",bleHeartRate);
+  RED.nodes.registerType("ble-heartrate",bleHeartrate);
 }
-
 
 
 
